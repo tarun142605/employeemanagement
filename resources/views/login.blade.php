49b75437846f67a1b9b2264c800fe728410806b8
tarun142605 <tarun142605@gmail.com>
2023-02-11 01:19:25 +0530

    added necessary files

---
+++ resources/views/login.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{url('/')}}/assets/css/bootstrap.min.css">
+</head>
+<body>
+
+    <h1>Hello</h1>
+    
+    <script src="{{url('/')}}/assets/js/jquery-3.3.1.min.js"></script>
+    <script src="{{url('/')}}/assets/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
